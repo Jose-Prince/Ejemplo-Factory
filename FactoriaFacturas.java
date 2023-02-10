d1108b7c185279777f43101fe87f8148f10f6822
Jose-Prince <joseprince148mail.com>
2023-02-10 11:37:45 -0600

    FacturaFactorias

--- FactoriaFacturas.java
+++ FactoriaFacturas.java
@@ -1,21 +1,15 @@
 public class FactoriaFacturas {
-    
-}
- /**
-  * 
-  * @param tipo
-  public static Factura getFactura(String tipo) {
-   
-   if (tipo.equals("iva")) {
-   
-   return new FacturaIva();
-   }
-   else {
-   //return new FacturaIvaReducido();
-   }
-   
-   }
-  }
-  * @return
-  */
+    public static Factura getFactura(String tipo) {
+     
+     if (tipo.equals("iva")) {
+     
+     return new FacturaIva();
+     }
+     else {
+     //return new FacturaIvaReducido();
+     }
+    return new FacturaIvaReducido();
+     
+     }
+    }
 
--- Principal.java
+++ Principal.java
@@ -3,7 +3,7 @@ public class Principal {
  
  public static void main(String[] args) {
  
- Factura f= FactoriaFacturas.getFactura("iva");
+ Factura f = FactoriaFacturas.getFactura("iva");
  f.setId(1);
  f.setImporte(100);
  System.out.println(f.getImporteIva());
