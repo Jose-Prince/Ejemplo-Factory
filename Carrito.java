4033a819cccde23b6c182be5f5ab6ac2c00d393f
Andre <Andrewlol774@gmail.com>
2023-02-10 12:07:45 -0600

    SIUU
    Signed-off-by: Andre <Andrewlol774@gmail.com>

--- Carrito.java
+++ Carrito.java
@@ -1,13 +1,68 @@
 public class Carrito {
-    int precioProducto1;
-    int precioProducto2;
-    int precioProducto3;
+    public int precioProducto1;
+    public int precioProducto2;
+    public int precioProducto3;
 
 
     public Carrito() {
-        precioProducto1 = 20;
-        precioProducto2 = 100;
-        precioProducto3 = 75;
+        int precioProducto1;
+        int precioProducto2;
+        int precioProducto3;
     }
+
+
+
+
+
+    public Carrito(int precioProducto1, int precioProducto2, int precioProducto3) {
+        this.precioProducto1 = precioProducto1;
+        this.precioProducto2 = precioProducto2;
+        this.precioProducto3 = precioProducto3;
+    }
+
+    public int getPrecioProducto1() {
+        return this.precioProducto1;
+    }
+
+    public void setPrecioProducto1(int precioProducto1) {
+        this.precioProducto1 = precioProducto1;
+    }
+
+    public int getPrecioProducto2() {
+        return this.precioProducto2;
+    }
+
+    public void setPrecioProducto2(int precioProducto2) {
+        this.precioProducto2 = precioProducto2;
+    }
+
+    public int getPrecioProducto3() {
+        return this.precioProducto3;
+    }
+
+    public void setPrecioProducto3(int precioProducto3) {
+        this.precioProducto3 = precioProducto3;
+    }
+
+    public Carrito precioProducto1(int precioProducto1) {
+        setPrecioProducto1(precioProducto1);
+        return this;
+    }
+
+    public Carrito precioProducto2(int precioProducto2) {
+        setPrecioProducto2(precioProducto2);
+        return this;
+    }
+
+    public Carrito precioProducto3(int precioProducto3) {
+        setPrecioProducto3(precioProducto3);
+        return this;
+    }
+
+
+
+
+
+  
 
 }
---
+++ carritos.java
@@ -0,0 +1,23 @@
+public class carritos {
+ 
+public Carrito carrito;
+
+public carritos(){
+    Carrito carrito = new Carrito();
+    carrito.setPrecioProducto1(0);
+    
+
+
+}
+
+
+
+
+
+    
+
+
+
+
+
+}
