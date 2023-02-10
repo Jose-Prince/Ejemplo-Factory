6dffdd02618e3e2ec8056a286805c1b620348241
Andre <Andrewlol774@gmail.com>
2023-02-10 12:10:23 -0600

    Signed-off-by: Andre <Andrewlol774@gmail.com>
    SIUUU

--- Principal.java
+++ Principal.java
@@ -4,9 +4,16 @@ public class Principal {
  public static void main(String[] args) {
  
  Factura f = FactoriaFacturas.getFactura("iva");
+ carritos carrito = carritaso;
  f.setId(1);
  f.setImporte(100);
  System.out.println(f.getImporteIva());
+
+ System.out.println("Reseteando...");
+
+
  }
+
+
  
 }
