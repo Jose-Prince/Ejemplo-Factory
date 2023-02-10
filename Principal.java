3db6b351383c458147beda7b61d3b3ebcd0827da
Ricardo Josué Morales Contreras <ricardo.josue44@gmail.com>
2023-02-10 11:32:06 -0600

    Create Principal.java
    Se crea la clase principal

---
+++ Principal.java
@@ -0,0 +1,12 @@
+ 
+public class Principal {
+ 
+ public static void main(String[] args) {
+ 
+ Factura f= FactoriaFacturas.getFactura("iva");
+ f.setId(1);
+ f.setImporte(100);
+ System.out.println(f.getImporteIva());
+ }
+ 
+}
